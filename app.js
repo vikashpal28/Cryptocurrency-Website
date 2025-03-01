e9088bc913014eb29f0e3c6a4b96bf97762811b2
vikashpal28 <vp537285@gmail.com>
2025-03-01 19:10:24 +0530

    CryptoCurrency Website

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const url = 'https://coingecko.p.rapidapi.com/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd';
+const url =
+  "https://coingecko.p.rapidapi.com/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd";
 const options = {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': '9958c92e3fmsh232ed284cbab050p16273bjsn37875020a191',
-        'x-rapidapi-host': 'coingecko.p.rapidapi.com'
-    }
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "9958c92e3fmsh232ed284cbab050p16273bjsn37875020a191",
+    "x-rapidapi-host": "coingecko.p.rapidapi.com",
+  },
 };
 
 async function fetchCryptoPrice() {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        document.getElementById('bitcoin_price').innerHTML = "$"+result.bitcoin.usd;
-        document.getElementById('ethereum_price').innerHTML = "$"+result.ethereum.usd;
-        document.getElementById('dogecoin_price').innerHTML = "$"+result.dogecoin.usd;
-
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    document.getElementById("bitcoin_price").innerHTML =
+      "$" + result.bitcoin.usd;
+    document.getElementById("ethereum_price").innerHTML =
+      "$" + result.ethereum.usd;
+    document.getElementById("dogecoin_price").innerHTML =
+      "$" + result.dogecoin.usd;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 fetchCryptoPrice();
